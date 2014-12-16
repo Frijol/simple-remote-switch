edae73c968d8242def504e42d47bba19ca2f386a
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-16 13:15:38 -0800

    added ready notifications to make push-ready

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('tesselate') ({
     C: ['relay-mono', 'relay']
   }
 }, function (tessel, modules) {
+  console.log('Ready to be remote controlled.');
+  tessel.led[0].write(1); // Turn on green light to show readiness
   modules.ir.on('data', function () {
     modules.relay.toggle(1);
   });
